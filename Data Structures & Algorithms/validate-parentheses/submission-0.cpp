73da828f664633f77c4a6a5e87aef399c6489d09
Abhinav Aakash <137511962+2-BrainCells@users.noreply.github.com>
2026-05-11 21:14:08 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> temp;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                temp.push(c);
+            } else {
+                if (temp.empty()) return false;
+
+                char top = temp.top();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') ||
+                    (c == '}' && top == '{')) {
+                    temp.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return temp.empty();
+    }
+};
