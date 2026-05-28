152a9c4824582266cb47eb9c5e62f767a5692628
Abhinav Aakash <137511962+2-BrainCells@users.noreply.github.com>
2026-05-28 13:54:55 +0530

    Add: linked-list-cycle-detection - submission-8

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-8.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool hasCycle(ListNode* head) {
+        ListNode *slow, *fast;
+        slow = head, fast = head;
+        // if(head == nullptr){
+        //     return 0;
+        // }
+        // if(head->next == nullptr){
+        //     return 0;
+        // }
+        while(fast && fast->next) {
+            slow = slow->next;
+            if(fast->next->next == nullptr) return 0;
+            else fast = fast->next->next;
+            if(slow == fast){
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
